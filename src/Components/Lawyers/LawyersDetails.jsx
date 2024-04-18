e5370b50976a924d431fe4a1970b491bfff425ab
safaaabdelnaser <safaaabdelnaser76@gmail.com>
2024-04-18 05:12:47 +0200

    update

--- src/Components/Lawyers/LawyersDetails.jsx
+++ src/Components/Lawyers/LawyersDetails.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import "../SingleType/SingleType.css";
+import "./Lawyers.css";
 
 const LawyerDetails = ({ lawyer }) => {
   const { categoryId } = useParams();
